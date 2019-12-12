80324644e11c7d1220ecf7764c7019bed6b5322c
Erik Kristofer Anderson <erik.kristofer.anderson@gmail.com>
2019-12-11 21:59:43 -0600

    add unbalanced_example/ (for later)

---
+++ unbalanced_example/main.py
@@ -0,0 +1,24 @@
+# from unbalanced_example.Node import Node
+from unbalanced_example.Tree import Tree
+
+import random
+
+
+my_tree = Tree()
+for i in range(8):
+    my_tree.add_node_by_value(random.randint(1, 100))
+    #my_tree.add_node_by_value(i)
+
+
+# my_tree.pretty_print()
+
+my_tree.print_in_order_traversal()
+my_tree.pretty_print()
+
+
+my_tree.rotate_left(my_tree.root)
+
+# my_tree.pretty_print()
+
+my_tree.print_in_order_traversal()
+my_tree.pretty_print()
